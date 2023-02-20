22ce814a86982d6370998f3aada606bb5973a436
Marlu.Patrocinio <marlupatrocinio01@gmail.com>
2023-02-20 10:14:07 -0300

    commit

---
+++ src/entities/OutsourcerdEmployee.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class OutsourcerdEmployee extends Employee {
+
+	private Double additionalCharge;
+
+	public OutsourcerdEmployee() {
+		super();
+	}
+
+	public OutsourcerdEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+
+	@Override
+	public Double payment() {
+
+		return super.payment() + additionalCharge * 1.1;
+	}
+
+
+}
